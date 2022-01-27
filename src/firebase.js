232a14539f74d17daeef081d37246d001741980a
KaushikKC <kccreations1704@gmail.com>
2022-01-27 19:26:14 +0530

    Setup the folder

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBu_-xrJVauNg0gFNSB4ky4aUivtuDvLwA",
+    authDomain: "netflix-clone-efcd3.firebaseapp.com",
+    projectId: "netflix-clone-efcd3",
+    storageBucket: "netflix-clone-efcd3.appspot.com",
+    messagingSenderId: "99107609129",
+    appId: "1:99107609129:web:58735e81296bee5cd4da38"
+  };
